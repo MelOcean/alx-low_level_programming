e86012f033c70b5794f73bc21efaa7a43d47d159
MelOcean <melba.batho@gmail.com>
2023-06-05 10:03:20 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,4 +9,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n);
 {
-	listint_t
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (Null);
+
+		new-n> = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
