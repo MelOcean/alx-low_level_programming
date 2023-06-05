92f5decc5c5f51ace40ddc3a15c44d658579425b
MelOcean <melba.batho@gmail.com>
2023-06-05 08:31:36 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: the linked list of the type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h);
+{
+	size_t num = 0
+		while (h)
+		{
+			h = h->next;
+		}
+	Return (num);
+}
