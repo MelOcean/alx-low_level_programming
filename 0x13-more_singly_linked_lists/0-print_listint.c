4c6c3762a09a8696faa42cea336ccf9bf8d5aef2
MelOcean <melba.batho@gmail.com>
2023-06-05 09:35:16 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,18 @@
  *
  * Return: the number of nodes
  **/
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 
 {
 
 	size_t num = 0;
-	
+
 	while (h)
 	{
 		printf("%i\n", h->n);
 		num++;
 		h = h->next;
 	}
-	Return (num);
+
+	return (num);
 }
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function that adds a new node at the beginning of a listint_t list
+ * @head: points to first node in the linked list
+ * @n: information to add the new node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n);
+{
+	listint_t
