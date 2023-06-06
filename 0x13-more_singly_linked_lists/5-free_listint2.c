871f48a6e871440e7b476f3f02af5163a1c59f5a
MelOcean <melba.batho@gmail.com>
2023-06-06 13:52:50 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head);
 {
 	listint_t *temp;
 
-	if (head == NULL)
+	if (*head == NULL)
 	return;
 	while (*head)
 	{
@@ -16,5 +16,6 @@ void free_listint2(listint_t **head);
 		free(*head);
 		*head = temp;
 	}
+
 	*head = NULL;
 }
