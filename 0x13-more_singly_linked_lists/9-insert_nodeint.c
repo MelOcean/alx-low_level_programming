d34a77a50182954deeb443aff4e4596f169387c8
MelOcean <melba.batho@gmail.com>
2023-06-06 15:26:15 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c 9-insert_nodeint.c -o j

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that inserts a new node at given position
+ * @head: pointer to first node in list
+ * @idx: index list where new node is added
+ * @n: input to insert new node
+ *
+ * Return: address of new node, or NULL if it fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned in d;
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	if (d == idx - 1)
+	{
+		new->next = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
