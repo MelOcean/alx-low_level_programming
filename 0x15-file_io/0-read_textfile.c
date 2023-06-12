8cf16be22c776584b0eb319fec59438587bdacf0
MelOcean <melba.batho@gmail.com>
2023-06-12 11:28:27 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - unction that reads a text file and prints to standard output
+ * @filename: file that will be read
+ * @letter: number of the letters that will be read
+ * Return: actual number of letters it could read and printed
+ *	if fails or NULL return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssze_t r;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == --1)
+		return (0);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+0x15. C - File I/O
+Dated 12 June 2023
